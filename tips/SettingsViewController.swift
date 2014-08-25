5d0adfa1b7a5b80ce69a0b1d04b318a76422828b
vjkaruna <vjkaruna@gmail.com>
2014-08-24 23:51:30 -0700

    Checkin with settings.

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var percentSwitch: UISwitch!
+    @IBOutlet weak var shakeSwitch: UISwitch!
+    @IBOutlet weak var localePicker: UIPickerView!
+    
+    var _localeData: [String] = ["Default", "Japan", "China", "Italy", "France", "Australia", "United States"]
+    var _localeCodes: [String] = ["en_US", "jp_JP", "zh_CN", "it_IT", "fr_FR",  "en_AU", "en_US"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.localePicker.dataSource = self
+        self.localePicker.delegate = self
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        percentSwitch.on = defaults.boolForKey("more_tips")
+        shakeSwitch.on = defaults.boolForKey("shake")
+        
+        var locale_row = 0
+        let default_locale = defaults.objectForKey("locale_code") as String
+        
+        if let def_row = find(_localeCodes, default_locale) {
+            localePicker.selectRow(def_row, inComponent: 0, animated: true)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +44,30 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /*
+    // MARK: - Picker Delegate
+    */
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int {
+        return _localeData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
+        return _localeData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int) {
+        let codeChoice = _localeCodes[row]
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(codeChoice, forKey: "locale_code")
+        defaults.synchronize()
+    }
 
+    
     /*
     // MARK: - Navigation
 
@@ -34,5 +80,13 @@ class SettingsViewController: UIViewController {
 
     @IBAction func tapAction(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: {})
+    }
+    @IBAction func shakeAction(sender: AnyObject) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool((sender as UISwitch).on, forKey: "shake")
+    }
+    @IBAction func percentChanged(sender: AnyObject) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool((sender as UISwitch).on, forKey: "more_tips")
     }
 }
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -21,32 +21,131 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
+        billField.text = ""
+        
+        billField.keyboardType = .DecimalPad
+        
+        setSegmentValues()
+        setLabelValues()
     
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        setSegmentValues()
+        calcTotal()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setLabelValues() {
+        var nf: NSNumberFormatter = getNumberFormatter()
+        tipLabel.text = nf.stringFromNumber(0)
+        totalLabel.text = nf.stringFromNumber(0)
+    }
+    
+    func setSegmentValues() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var more_preference = defaults.boolForKey("more_tips")
+        if more_preference && tipControl.numberOfSegments < 4  {
+            tipControl.insertSegmentWithTitle("30%", atIndex: 3, animated:true)
+        }
+        if !more_preference && tipControl.numberOfSegments > 3 {
+            tipControl.removeSegmentAtIndex(3, animated: true)
+        }
+    }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18, 0.2, 0.22]
+        calcTotal()
+    }
+    
+    func calcTotal() {
+        var tipPercentages = [0.15, 0.2, 0.25, 0.3]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         println("User editing object")
         
-        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        // Use locale-specific Number formatter
+        var nf: NSNumberFormatter = getNumberFormatter()
+        
+        var billAmount: Double = 0.0
+        
+        if let ba_locale = nf.numberFromString(billField.text)?.doubleValue  {
+            billAmount = ba_locale
+        } else {
+            billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        }
+        
+        //var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
+        // Optionally initialize the property to a desired starting value
+        tipLabel.alpha = 0
+        totalLabel.alpha = 1
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text = nf.stringFromNumber(tip)
+        totalLabel.text = nf.stringFromNumber(total)
+        //totalLabel.bounds = CGRectMake(124, 299, 180, 30)
+        
+        var anim = POPBasicAnimation(propertyNamed: "view.alpha")
+        anim.timingFunction = CAMediaTimingFunction(name: "easeInEaseOut")
+        anim.fromValue = 0
+        anim.toValue = 1
+        var slide_anim = POPBasicAnimation(propertyNamed: "view.center")
+        anim.timingFunction = CAMediaTimingFunction(name: "easeInEaseOut")
+        //slide_anim.fromValue = NSValue(CGRect: CGRectMake(124, 299, 180, 30))
+        //slide_anim.toValue = NSValue(CGRect: CGRectMake(124, 233, 180, 30))
+        slide_anim.fromValue = NSValue(CGPoint: CGPoint(x: 214,y: 299))
+        slide_anim.toValue = NSValue(CGPoint: CGPoint(x: 214,y: 248))
+        self.totalLabel.pop_addAnimation(slide_anim, forKey: "slide")
+        self.totalLabel.pop_addAnimation(anim, forKey: "fade")
+        self.tipLabel.pop_addAnimation(anim, forKey: "fade")
+        
+
+        /*
+            UIView.animateWithDuration(1, animations: {
+                self.tipLabel.alpha = 1
+            })
+        */
+        
+    
+        
+        
+        //tipLabel.text = String(format: "$%.2f", tip)
+        //totalLabel.text = String(format: "$%.2f", total)
     }
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
+    }
+    
+    func getNumberFormatter() -> NSNumberFormatter {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var locale_code = defaults.objectForKey("locale_code") as String
+        if locale_code == "" { locale_code = "en_US" }
+        var nf: NSNumberFormatter = NSNumberFormatter()
+        nf.numberStyle  = .CurrencyStyle
+        nf.locale = NSLocale(localeIdentifier: locale_code)
+        return nf
+    }
+    
+    // detect shake events
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent!) {
+        if (motion == .MotionShake) {
+            var defaults = NSUserDefaults.standardUserDefaults()
+            var shake_preference = defaults.boolForKey("shake")
+            if shake_preference {
+                tipControl.selectedSegmentIndex = Int(arc4random_uniform(UInt32(tipControl.numberOfSegments)))
+            }
+        }
     }
 
 
